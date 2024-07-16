e772fac48fd6ccfded7fcc154c39d62e73ae8873
Matteo <matteo.courquin@fortuneo.com>
2024-07-16 17:28:44 +0200

    :construction: Make * Pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,29 @@
+import Layout from '@/layout/default';
 import SmoothScrolling from '@/layout/lenis';
 import '@/styles/main.scss';
 import type { AppProps } from 'next/app';
 import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const pathname = usePathname();
+  const [layoutEnabled, setLayoutEnabled] = useState(true);
+
+  useEffect(() => {
+    setLayoutEnabled(!pathname?.includes('studio'));
+  }, [pathname]);
 
   return (
-    <SmoothScrolling>
-      <Component key={pathname} {...pageProps} />
-    </SmoothScrolling>
+    <>
+      {layoutEnabled ? (
+        <SmoothScrolling>
+          <Layout>
+            <Component key={pathname} {...pageProps} />
+          </Layout>
+        </SmoothScrolling>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
+import Button, { BUTTON_SIZE } from '@/components/atoms/Button';
+import { IconArrowTopRight } from '@/components/atoms/Icons';
+import Typography, { TYPOGRAPHY_TYPE } from '@/components/atoms/Typography';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Matteo Courquin</h1>
-    </main>
+    <>
+      <section className="bg-gradient flex h-screen w-screen flex-col items-center justify-center text-white">
+        <Typography type={TYPOGRAPHY_TYPE.HEADING1} className="text-center">
+          Hey👋 I'm
+        </Typography>
+        <Typography type={TYPOGRAPHY_TYPE.HEADING2} className="text-center">
+          Matteo COURQUIN
+        </Typography>
+        <Typography type={TYPOGRAPHY_TYPE.SUBTITLE}>WEB DEVELOPER ( SINCE 2019 )</Typography>
+        <Button size={BUTTON_SIZE.L} as="button">
+          Start Project <IconArrowTopRight className="ml-2 h-4" />
+        </Button>
+      </section>
+    </>
   );
 }
