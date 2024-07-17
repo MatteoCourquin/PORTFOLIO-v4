4cd195ecfe8b739ceeaadae9b341ddd53876ea7b
Matteo <matteo.courquin@fortuneo.com>
2024-07-17 19:39:15 +0200

    :construction: Make Hero

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: "#ffffff",
+        black: "#181818",
+      },
+      padding: {
+        'x-default': "clamp(20px, 8vw, 100px)",
+        'y-default': "clamp(20px, 8vh, 100px)",
+      },
+      inset: {
+        'x-default': "clamp(20px, 8vw, 100px)",
+        'y-default': "clamp(20px, 8vh, 100px)",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
