f2ef3307e701e44bc016e686a01366c2e3512a26
Matteo <matteo.courquin@fortuneo.com>
2024-07-29 19:37:46 +0200

    :bug: Fix burger

--- layout/default.tsx
+++ layout/default.tsx
@@ -1,12 +1,11 @@
 import Burger from '@/components/Burger';
 import Footer from '@/components/Footer';
-import Header from '@/components/Header';
 import { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Burger />
       <main>{children}</main>
       <Footer />
