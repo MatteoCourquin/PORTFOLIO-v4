2fbf05f8ff46b0a2cecbb269836523c2d4b6a327
Matteo <matteo.courquin@fortuneo.com>
2024-08-15 08:01:36 +0200

    :iphone: Imrove responsive

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,4 +13,6 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig);
