d32090e4142c9b8a1d96bfca723d895feb08fbcf
Matteo <matteo.courquin@fortuneo.com>
2024-10-01 15:09:54 +0200

    :lipstick: SCSS to SASS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,33 @@
 import Layout from '@/layout/default';
 import SmoothScrolling from '@/layout/lenis';
-import '@/styles/main.scss';
-import { useLenis } from '@studio-freight/react-lenis';
+import '@/styles/main.sass';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+// import { useLenis } from '@studio-freight/react-lenis';
+// import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const pathname = usePathname();
-  const router = useRouter();
-  const lenis = useLenis();
+  // const router = useRouter();
+  // const lenis = useLenis();
 
-  useEffect(() => {
-    const handleRouteChange = () => {
-      if (!lenis) return;
+  // useEffect(() => {
+  //   const handleRouteChange = () => {
+  //     if (!lenis) return;
 
-      lenis.scrollTo(lenis.actualScroll);
+  //     lenis.scrollTo(lenis.actualScroll);
 
-      setTimeout(() => {
-        lenis.scrollTo(0, {
-          immediate: true,
-        });
-      }, 800);
-    };
+  //     setTimeout(() => {
+  //       lenis.scrollTo(0, {
+  //         immediate: true,
+  //       });
+  //     }, 800);
+  //   };
 
-    router.events.on('routeChangeStart', handleRouteChange);
-  }, [router.events, lenis, pathname]);
+  //   router.events.on('routeChangeStart', handleRouteChange);
+  // }, [router.events, lenis, pathname]);
 
   return (
     <>
