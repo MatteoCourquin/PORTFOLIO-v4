afa9657c3016438389a2afa6c3f991aba79e7adb
Matteo <matteo.courquin@fortuneo.com>
2024-10-04 17:56:24 +0200

    :bug: Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {pathname?.includes('studio') ? (
-        <Component {...pageProps} />
+        <Component key={pathname} {...pageProps} />
       ) : (
         <Layout>
           <SmoothScrolling>
